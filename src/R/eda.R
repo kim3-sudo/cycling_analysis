068dea209f77bd346fd1e6b6ae41a7b3d5bb9363
Sejin Kim <kim3@kenyon.edu>
2021-04-24 17:32:28 -0400

    Added R project and read data into EDA file

---
+++ src/R/eda.R
@@ -0,0 +1,7 @@
+### Cycling Analysis
+### Josh Katz & Sejin Kim
+### STAT 306 S21 @ Kenyon College
+
+tourOfTheAlps <- readRDS(url('https://github.com/kim3-sudo/cycling_analysis_data/blob/main/rds/tour_of_the_alps.rds?raw=true'))
+tourOfFlanders <- readRDS(url('https://github.com/kim3-sudo/cycling_analysis_data/blob/main/rds/tour_of_flanders.rds?raw=true'))
+
